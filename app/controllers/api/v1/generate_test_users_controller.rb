b970ba564c63e365dcd732de4d85d6cf1d17b99d
Naoki Kawamoto <kevinriverbook@gmail.com>
2023-10-05 10:32:24 +0900

    feat: テストユーザーを生成するコントローラを作成

---
+++ app/controllers/api/v1/generate_test_users_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::GenerateTestUsersController < ApplicationController
+  def generate_test_users
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'generate_test_users/generate_test_users'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
