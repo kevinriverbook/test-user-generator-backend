6dca2a5b7f282655ccc05669c9b4639ccdb20acf
Naoki Kawamoto <kevinriverbook@gmail.com>
2023-10-06 20:31:53 +0900

    feat: 生年月日機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'generate_test_users/generate_test_users'
+      get 'generate_test_users/', to: 'generate_test_users#generate_test_users'
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
