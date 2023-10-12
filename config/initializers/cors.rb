9c5074421810ddb541b75df0f0e77a22d9d7ee2a
Naoki Kawamoto <kevinriverbook@gmail.com>
2023-10-12 12:42:07 +0900

    originsを修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,6 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins "https://testusergenerator.vercel.app/", "localhost:3000"
+    origins "*"
 
     resource "*",
       headers: :any,
